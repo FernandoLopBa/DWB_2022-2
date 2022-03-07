48771a9d364e0ae1d73d8b75ba52ef20fe522d68
FernandoLB <fernandolb@ciencias.unam.mx>
2022-03-06 21:26:10 -0600

    Añadiendo entidad Category

---
+++ practica_03/src/main/java/com/practica03/category/api/entity/Category.java
@@ -0,0 +1,68 @@
+package com.practica03.category.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Integer category_id;
+	
+	@NotNull
+	@Column(name = "category")
+	private String category;
+	
+	@Column(name = "status")
+	@Min(value = 0, message = "status must be 0 or 1")
+	@Max(value = 1, message = "status must be 0 or 1")
+	@JsonIgnore
+	private Integer status;
+	
+	public Category(int category_id, String category, int status) {
+		this.category_id = category_id;
+		this.category = category;
+		this.status = status;
+	}
+
+	public Integer getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(Integer category_id) {
+		this.category_id = category_id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [category_id=" + category_id + ", category=" + category + ", status=" + status + "]";
+	}
+}
